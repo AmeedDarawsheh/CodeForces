5da2a7b4930c1afac7c97382630e3bc8c70acbea
Ameed Darawsheh <ce.ameed@gmail.com>
2025-07-04 11:00:13 +0300

    Add solution for problem 579A in C++

---
+++ cpp/579A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x;
+    cin >> x;
+    int count = 0;
+
+    while (x > 0)
+    {
+        if (x % 2 == 1)
+            count++;
+        x /= 2;
+    }
+    cout << count << "\n";
+
+    return 0;
+}
